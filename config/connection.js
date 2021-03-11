b50dd1c595cfd92b5c5d30e3e72b99e43e4e058e
Mike Voynovich <mvoyno@gmail.com>
2021-03-11 14:26:53 -0500

    update connection

--- config/connection.js
+++ config/connection.js
@@ -2,20 +2,26 @@ const mysql = require("mysql");
 const dotenv = require("dotenv");
 const util = require("util");
 dotenv.config();
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "Th1$SQLPW",
+    database: "burgers_db"
+  });
+}
 
-var connection = mysql.createConnection({
-  host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
-
-  // Your username
-  user: "root",
-
-  // Your password
-  password: "Th1$SQLPW",
-  database: "burgers_db"
-});
 
 connection.connect(function(err) {
   if (err) throw err;
